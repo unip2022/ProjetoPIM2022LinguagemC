77fc13eae0a587d96268ff8ea5160763a93205ab
TiagoDat1012 <atendimento@wssh.com.br>
2022-08-27 21:51:49 -0300

    27/08/22 - Login parte 2

--- main.c
+++ main.c
@@ -1,15 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int CadastroClientes(){
+    system("cls");
+    printf("=== CADASTRO DE CLIENTES ===");
+}
+
+int CadastroFuncionarios(){
+    system("cls");
+    printf("=== CADASTRO DE FUNCIONARIOS ===");
+
+}
+
+int CadastroPefil(){
+    system("cls");
+    printf("=== CADASTRO DE PERFIL ===");
+
+}
 
 int Menu(){
-    int menu;
+    char menu[61];
     system("cls");
     printf("ESCOLHA UMA OPCAO: \n 1 - CADASTRO CLIENTES \n 2 - CADASTRO DE FUNCIONARIOS \n 3 - CADASTRO DE PERFIL \n 4 - SAIR ");
 
-    printf("\n:Digite sua escolha: ");
-    scanf("%i",menu);
+    printf("\nDigite sua escolha: ");
+    scanf("%s",menu);
+    printf("\nSua escolha: %s",menu);
 
+
+    if( strcmp(menu, "1") == 0){
+        CadastroClientes();
+    }
+
+    if( strcmp(menu, "2") == 0){
+        CadastroFuncionarios();
+    }
+
+    if( strcmp(menu, "3") == 0){
+        CadastroPefil();
+    }
+
+    if( strcmp(menu, "4") == 0){
+        return 0;
+    }
+    /*
+    switch(menu){
+    case 1 :
+        CadastroClientes();
+    break;
+    }
+    */
+
+    return menu;
 }
 
 
@@ -21,6 +63,7 @@ int main(void)
     char senha[61];
     int i;
 
+    //Menu();
 
     /// *** ENTRADA DE EMAIL
     EMAIL: ///  INICIO DO GOTO
@@ -54,6 +97,6 @@ int main(void)
         goto EMAIL;
     }
 
-    getch();
+    //getch();
     return 0;
 }
